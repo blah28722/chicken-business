0d15b36238abf42ee3168dc06496797210e0657f
John Chen <john.hk.chen@gmail.com>
2023-12-24 17:19:10 -0800

    initialized skeleton project

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { chicken_business } from './src/chicken_business'
+
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		forms,
+		typography,
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'skeleton',
+						enhancements: true,
+					},
+				],
+				custom: [
+					chicken_business,
+				],
+			},
+		}),
+	],
+} satisfies Config;
