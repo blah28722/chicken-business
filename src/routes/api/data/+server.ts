a36e68ef6794642e9be853f04c5d8ee0c9ad729c
John Chen <john.hk.chen@gmail.com>
2023-12-26 08:25:11 -0800

    Try to allow CORS requests

--- src/routes/api/data/+server.ts
+++ src/routes/api/data/+server.ts
@@ -1,5 +1,15 @@
 /** @type {import('./$types').RequestHandler} */
 
+export async function OPTIONS() {
+  return new Response(null, {
+    headers: {
+      'Access-Control-Allow-Origin': 'https://blah28722.github.io',
+      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  })
+}
+
 export function GET({ request }) {
     console.log(request);
     const options: ResponseInit = {
